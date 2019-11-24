981d5f78134240db2c49c0eb2deed45c20977151
Vladislav <egorov.vladislav@outlook.com>
2019-11-24 19:17:21 +0300

    Initial commit

---
+++ src/ru/mtu/Main.java
@@ -0,0 +1,54 @@
+package ru.mtu;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    /**
+     * Задача 7. В целочисленной матрице А размером NyM заменить
+     * элементы главной диагонали и расположенные ниже нее на значение
+     * 3 (числа N и М задаются с клавиатуры в диапазоне от 3 до 10).
+     * Первоначальное заполнение массива А. осуществить при помощи датчика
+     * случайных чисел в диапазоне от -5 до 15.
+     * Исходную и видоизмененную матрицы вывести на экран.
+     */
+    public static void main(String[] args) {
+        initRandomMatrix(10, 5, 3);
+    }
+
+    public static void initRandomMatrix(int n, int m, int numberFoReplacement) {
+        int[][] resultMatrix = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                resultMatrix[i][j] = ThreadLocalRandom.current().nextInt(-5, 16);
+            }
+        }
+        System.out.println("Исходная матрица: ");
+        printMatrix(resultMatrix);
+
+        int mainDCounter = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (j <= mainDCounter) {
+                    resultMatrix[i][j] = numberFoReplacement;
+                    if (j == mainDCounter) {
+                        mainDCounter++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Матрица после преобразований: ");
+        printMatrix(resultMatrix);
+
+    }
+
+    public static void printMatrix(int mat[][]) {
+        for (int[] row : mat) {
+            System.out.println(Arrays.toString(row));
+
+        }
+        System.out.println("\n");
+    }
+}
---
+++ src/ru/mtu/Main170.java
@@ -0,0 +1,56 @@
+package ru.mtu;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main170 {
+
+    /**
+     * Задача 7. Вывести на экран все натуральные шестизначные числа
+     * из диапазона от А до В, у которых совпадают сумма трех младших и
+     * трех старших цифр. При отсутствии чисел с указанными свойствами
+     * сформировать сообщение «Требуемых чисел нет».
+     * Границы диапазона А и В вводятся с клавиатуры.
+     */
+    public static void main(String[] args) {
+        List<Integer> list = generateListByRange(1000, 555555);
+        System.out.println(list);
+        List<Integer> cleanedList = cleanIntList(list);
+        System.out.println(cleanedList);
+    }
+
+    /**
+     * @param list с элементами, которые нужно отфильтровать
+     * @return список чисел, у которых совпадают первые 3 и последние 3 цифры
+     */
+    private static List<Integer> cleanIntList(List<Integer> list) {
+        return list.stream().filter(integer ->
+                sumOFNumbers(integer / 1000).equals(sumOFNumbers(integer % 1000)))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @param low нижняя границы диапазона (включительно)
+     * @param high верхняя границы диапазона (включительно)
+     * @return
+     */
+    private static List<Integer> generateListByRange(Integer low, Integer high) {
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = low; i < high; i++) {
+            if (i >= 100000 && i <= 999999) {
+                integerList.add(i);
+            }
+        }
+        return integerList;
+    }
+
+    /**
+     * @param number натуральное положительное число
+     * @return сумма отдельных цифр числа
+     */
+    private static Integer sumOFNumbers(Integer number) {
+        return number == 0 ? 0 : number % 10 +
+                sumOFNumbers(number / 10);
+    }
+}
